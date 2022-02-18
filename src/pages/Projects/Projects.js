56e27a9e9169dfe9ac9155dbf28eda846fc2368d
Marouane <m.marouane14@gmail.com>
2022-02-18 19:33:42 +0100

    api-fetching

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -8,25 +8,64 @@ import imgproject from '../../assets/projectimg.jpeg';
 function Projects () {
     return (
         <>
-        <section className="container"> 
             <h1> MY PROJECTS</h1>
-            <div className="containerCards" >
-                <Card className="card" style={{ width: '18rem' }}>
-                    <Card.Img  className="img" variant="top" src={imgproject} width="180px" />
-                    <Card.Body>
+            <div className="containercards" >
+                <Card className="card" style={{ width: '41vh' }}>
+                    <Card.Img  className="img" variant="top" src={imgproject} width="180px"/>
+                    <Card.Body className="body">
+                        <div className="content"> 
                         <Card.Title className="title" >Card Title</Card.Title>
                         <Card.Text className="text">
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
                         </Card.Text>
-                        <Button className="button" variant="primary">Go somewhere</Button>
+                        <Card.Text className="text">
+                        <span> Date: </span>
+                        XX/XX/XXXX
+                        </Card.Text>
+                        </div>
+                        <Button className="button" variant="primary"> OPEN </Button>
+                    </Card.Body>
+                </Card>
+                <Card className="card" style={{ width: '41vh' }}>
+                    <Card.Img  className="img" variant="top" src={imgproject} width="180px"/>
+                    <Card.Body className="body">
+                        <div className="content"> 
+                        <Card.Title className="title" >Card Title</Card.Title>
+                        <Card.Text className="text">
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Card.Text className="text">
+                        <span> Date: </span>
+                        XX/XX/XXXX
+                        </Card.Text>
+                        </div>
+                        <Button className="button" variant="primary"> OPEN </Button>
+                    </Card.Body>
+                </Card>
+                <Card className="card" style={{ width: '41vh' }}>
+                    <Card.Img  className="img" variant="top" src={imgproject} width="180px"/>
+                    <Card.Body className="body">
+                        <div className="content"> 
+                        <Card.Title className="title" >Card Title</Card.Title>
+                        <Card.Text className="text">
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                    
+                        <Card.Text className="text">
+                        <span> Date: </span>
+                        XX/XX/XXXX
+                        </Card.Text>
+                        </div>
+                        <Button className="button" variant="primary"> OPEN </Button>
                     </Card.Body>
                 </Card>
             </div>
             <div>
                 
             </div>
-        </section>
         </>
     )
 }
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useHistory } from "react-router-dom";
 import styled from 'styled-components';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -6,50 +7,85 @@ import img2 from '../../assets/image2.png';
 import './Signup.css';
 
 function Signup() {
+
+    const [firstname,setFirstName]=useState("");
+    const [lastname,setLastName]=useState("");
+    const [username,setUserName]=useState("");
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    const [passwordConf,setPasswordConf]=useState("");
+    const [avatar,setAvatar]=useState("");
+    
+    const history = useHistory();
+
+    const Signup = async () => {
+        let user = {firstname,lastname, username,email,password,passwordConf,avatar};
+        console.warn(user);
+        
+        let result= await fetch("http://127.0.0.1:8000/api/register",{
+            method:'POST',
+            body:JSON.stringify(user),
+            headers:{
+                "Content-Type":"application/json",
+                "Accept":'application/json'
+            }
+        })
+
+        result = await result.json()
+        localStorage.setItem("user-info",JSON.stringify(result))
+        history.push ("/projects")
+    }
     return (
         <Wrapper className="container">
             <div className="divcol">
             <div className="divform">
-            <Form className="form">
-            <div className="group">
-            <div className="account">
-                <img src={img2} className="img2" />
-                <h2 className="labeltitle"> Create an account </h2>
-            </div>
+                <div className="group">
+                <div className="account">
+                    <img src={img2} className="img2" />
+                    <h2 className="labeltitle"> Create an account </h2>
+                </div>
+                
+                <div>
+                    <label> First Name </label>
+                    <input type="text" placeholder="Enter your first name" value={firstname}  onChange={(e)=>setFirstName(e.target.value)}/>
+                </div>
 
-            <Form.Group className="mb-3" controlId="formBasicName">
-                <Form.Label className="label">First Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter your first name" />
-            </Form.Group>
+                <div>
+                    <label> Last Name </label>
+                    <input type="text" placeholder="Enter your lastname name" value={lastname}  onChange={(e)=>setLastName(e.target.value)}/>
+                </div>
 
-            <Form.Group className="mb-3" controlId="formBasicName">
-                <Form.Label className="label">Last Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter your last name" />
-            </Form.Group>
+                <div>
+                    <label> Username </label>
+                    <input type="text" placeholder="Choose a username" value={username}  onChange={(e)=>setUserName(e.target.value)}/>
+                </div>
 
-            <Form.Group className="mb-3" controlId="formBasicName">
-                <Form.Label className="label">Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter your email" />
-            </Form.Group>
+                <div>
+                    <label> Email </label>
+                    <input type="email" placeholder="Enter your email" value={email}  onChange={(e)=>setEmail(e.target.value)}/>
+                </div>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label className="label">Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label className="label">Confirm your password </Form.Label>
-                <Form.Control type="password" placeholder="Confirm your password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Remember me " />
-            </Form.Group>
-                <Button variant="primary" type="submit">
-                Register
-                </Button>
-                <h5> Already Registered ? <a href="#"> Sign up </a></h5>
+                <div>
+                    <label> Password </label>
+                    <input type="password" placeholder="Enter your password" value={password}  onChange={(e)=>setPassword(e.target.value)}/>
+                </div>
 
-            </div>
-            </Form>
+                <div>
+                    <label>  Confirmation</label>
+                    <input type="password" placeholder="Confirm your password" value={passwordConf}  onChange={(e)=>setPasswordConf(e.target.value)}/>
+                </div>
+
+                <div>
+                    <label> Avatar </label>
+                    <input type="file"  value={avatar}  onChange={(e)=>setAvatar(e.target.value)}/>
+                </div>
+
+                <button onClick={Signup}>
+                    REGISTER
+                </button>
+                <h5> Already Registered ? <a href="#"> Login </a></h5>
+
+                </div>
             </div>
             </div>
         </Wrapper>
