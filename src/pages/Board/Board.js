13350007107adc7521eca4d7e28a70f5e63f4988
Marouane <m.marouane14@gmail.com>
2022-02-21 23:40:21 +0100

    board

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -1,39 +1,149 @@
-import React from "react";
-import './Board.css'
-import {BsPlusLg} from 'react-icons/bs';
-import Column from "../../components/Column/Column";
-import Sidebar from "../../components/Sidebar/Sidebar";
-import styled from "styled-components";
-import city from '../../assets/city.jpg';
+import React, { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-function Board() 
-{
-    return(
-        <Wrapper className="background" style={{ backgroundImage: "url('/city2.jpeg')" }}>
-        <div className="columns" >
-          <Sidebar/>
-          <Column title="To Do"> 
-          </Column>
 
-          <Column title="In Progress">
-          </Column>
+const itemsFromBackend = [
+  { id: 'a', content: "First task" },
+  { id: 'b', content: "Second task" },
+  { id: 'c', content: "Third task" },
+  { id: 'd', content: "Fourth task" },
+  { id: 'e', content: "Fifth task" }
+];
 
-          <Column title="Pending"> 
-          </Column>
+const columnsFromBackend = {
+  ['a']: {
+    name: "Requested",
+    items: itemsFromBackend
+  },
+  ['b']: {
+    name: "To do",
+    items: []
+  },
+  ['c']: {
+    name: "In Progress",
+    items: []
+  },
+  ['d']: {
+    name: "Done",
+    items: []
+  }
+};
 
-          <Column title="Done">
-          </Column> 
-        </div>
-        <div>
-          <button class="bn632-hover bn20"> <BsPlusLg/> ADD TASK </button>        
-        </div>
-        </Wrapper>
-    )
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems
+      }
+    });
+  } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems
+      }
+    });
+  }
+};
+
+function Board() {
+  const [columns, setColumns] = useState(columnsFromBackend);
+  return (
+    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
+      <DragDropContext
+        onDragEnd={result => onDragEnd(result, columns, setColumns)}
+      >
+        {Object.entries(columns).map(([columnId, column], index) => {
+          return (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center"
+              }}
+              key={columnId}
+            >
+              <h2>{column.name}</h2>
+              <div style={{ margin: 8 }}>
+                <Droppable droppableId={columnId} key={columnId}>
+                  {(provided, snapshot) => {
+                    return (
+                      <div
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        style={{
+                          background: snapshot.isDraggingOver
+                            ? "lightblue"
+                            : "lightgrey",
+                          padding: 4,
+                          width: 250,
+                          minHeight: 500
+                        }}
+                      >
+                        {column.items.map((item, index) => {
+                          return (
+                            <Draggable
+                              key={item.id}
+                              draggableId={item.id}
+                              index={index}
+                            >
+                              {(provided, snapshot) => {
+                                return (
+                                  <div
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                    style={{
+                                      userSelect: "none",
+                                      padding: 16,
+                                      margin: "0 0 8px 0",
+                                      minHeight: "50px",
+                                      backgroundColor: snapshot.isDragging
+                                        ? "#263B4A"
+                                        : "#456C86",
+                                      color: "white",
+                                      ...provided.draggableProps.style
+                                    }}
+                                  >
+                                    {item.content}
+                                  </div>
+                                );
+                              }}
+                            </Draggable>
+                          );
+                        })}
+                        {provided.placeholder}
+                      </div>
+                    );
+                  }}
+                </Droppable>
+              </div>
+            </div>
+          );
+        })}
+      </DragDropContext>
+    </div>
+  );
 }
-
-const Wrapper = styled.header`
-
-
-`
 
 export default Board;
